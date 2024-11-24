d0580b97fda17b26c38388ea8d78f9ac15955bd3
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2024-11-23 16:05:16 -0800

    Create P01.lisp

---
+++ P01.lisp
@@ -0,0 +1,13 @@
+;; Easiest way
+(defun last-element-reverse (x)
+  (car (reverse x)))
+
+;; Another very easy way 
+(defun last-element-last (x)
+  (car (last x)))
+
+;; Recursive way
+(defun last-element-recurse (x)
+  (if (null (cdr x))
+      (car x)
+      (last-element-recurse (cdr x))))
